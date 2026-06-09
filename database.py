e4ae9551fca92331973953982471c2cdfe98eb6e
Ramakant sharma <102178267+ramakant822382@users.noreply.github.com>
2026-06-09 09:40:51 +0530

    add cors and db

--- database.py
+++ database.py
@@ -1,8 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-# DATABASE_URL = "postgresql+psycopg2://cn_user:lkpeelmlhAbVZ2L6VMBteKCosPDZcLDH@dpg-d8imosl8nd3s73dovc4g-a/cn"
-DATABASE_URL = "postgresql://postgres:rama@localhost:5432/cn_project"
+DATABASE_URL = "postgresql+psycopg2://cn_user:lkpeelmlhAbVZ2L6VMBteKCosPDZcLDH@dpg-d8imosl8nd3s73dovc4g-a/cn"
+# DATABASE_URL = "postgresql://postgres:rama@localhost:5432/cn_project"
 engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 
 SessionLocal = sessionmaker(
